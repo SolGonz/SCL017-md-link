b15bbd20fad605711df334ea87d00c0e0ae3c9ae
Solange Gonzalez <solgonzalezaraya@gmail.com>
2021-09-09 12:53:43 -0300

    read file extrae links closes #4

--- index.js
+++ index.js
@@ -2,43 +2,13 @@
 //   // ...
 // };
 
-
-
 const fs = require('fs');
 const path = require('path');
 const markdownLinkExtractor = require('markdown-link-extractor');
 
 
-let ruta = 'test.md';
+let ruta = 'README.md';
 let absolutePath = path.resolve(ruta);
-
-const readFile = () => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(absolutePath, 'utf-8', (err, data) => {
-      if (err) {
-        reject(new Error('Error al leer el archivo'));
-      } if (path.extname(absolutePath) === '.md'){
-              console.log(data)
-      } else {
-        console.log('No es un archivo MD, ingresa una ruta válida')
-      }
-    });
-  });
-};
-
-// función que extrae archivos MD del directorio
-const readDir = () => {
-  fs.readdir(absolutePath, (err, files) =>{
-    if (err){
-      throw err;
-    }
-   files.forEach(file => { 
-     if (path.extname(file) === '.md'){
-       console.log('Este es un archivo MD del directorio: ', file)
-     }
-    });
-  });
-}
 
 const fileOrDir = () => {
   return new Promise((resolve, reject) => {
@@ -55,3 +25,37 @@ const fileOrDir = () => {
   });
 };
 
+
+// función que extrae archivos MD del directorio
+const readDir = () => {
+  fs.readdir(absolutePath, (err, files) =>{
+    if (err){
+      throw err;
+    }
+   files.forEach(file => { 
+     if (path.extname(file) === '.md'){
+       console.log('Este es un archivo MD del directorio: ', file)
+     }
+    });
+  });
+}
+
+
+const readFile = () => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(absolutePath, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Error al leer el archivo'));
+      } if (path.extname(absolutePath) === '.md'){
+        links = markdownLinkExtractor(data);
+        links = links.filter(link => ~link.indexOf("http"));
+              resolve(links);
+              console.log(links)
+      } else {
+        console.log('No es un archivo MD, ingresa una ruta válida')
+      }
+    });
+  });
+};
+
+fileOrDir();
